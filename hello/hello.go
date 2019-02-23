bf741ad854a88d2b5fead138a1b853126ea018d5
rightblank <fenius@163.com>
2019-02-23 14:40:13 +0800

    nothing new, just test

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+       "fmt"
+	   "github.com/rightblank/stringutil"
+)
+
+func main() {
+   fmt.Printf("hello, genius, Let's go!")
+   fmt.Printf(stringutil.Reverse("!og s'teL ,suineg ,olleh"))
+}
